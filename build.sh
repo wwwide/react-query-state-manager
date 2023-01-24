4e2e6e490f816460ba1cc9b7277088b52f7dddde
Andrey Barkanov <andrey.barkanov@gmail.com>
2023-01-24 21:58:44 +0300

    Fix typings

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ ENVIRONMENT=production ts-node ./esbuild/build.ts
 rm -rf ./dist/esbuild
 rm -rf ./dist/src/__tests__
 find ./dist/src -name "*.js" -type f -delete
+mv ./dist/src/* ./dist
+rm -rf ./dist/src
